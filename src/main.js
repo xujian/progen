b7d1c5c79677e264df89ec7b8e039bda7b59ca5b
xujian <782135@qq.com>
2018-12-18 18:54:24 +0800

    + quasar

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import './styles/quasar.styl'
+import iconSet from 'quasar-framework/icons/fontawesome'
+import 'quasar-extras/animate'
+import 'quasar-extras/fontawesome'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}, iconSet: iconSet
+})
+
 Vue.config.productionTip = false
 
 new Vue({
